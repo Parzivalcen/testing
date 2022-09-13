dfa161f1fd2101e7ab2304da032e683e682c9268
David Centurion <davidcenwork@gmail.com>
2022-09-13 12:59:28 -0400

    analize start

---
+++ testingPractice/analizeArray.test.js
@@ -0,0 +1,15 @@
+test('Analize', ()=>{
+  expect(analizeArray([1, 2, 3, 4, 5])).toEqual({
+    average: 3,
+    min: 1,
+    max: 5,
+    length: 5})
+})
+
+test('Analize (2)', ()=>{
+  expect(analizeArray([1,8,3,4,2,6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6})
+})
--- testingPractice/caesarCipher.js
+++ testingPractice/caesarCipher.js
@@ -1,35 +1,52 @@
 const caesarCipher = (str) => {
+  const strlen = str.length;
   const strChars = [];
-  let joined = [];
-  for(let i = 0; i < str.length; i++){
+  for(let i = 0; i < strlen; i ++){
     strChars.push(shift(str[i]));
-  };
-  
-  
+  }
   return strChars.join('');
 }
 
-function shift (letter){
-  let rotate;
+function shift(letter){
   if(isLetter(letter)){
-    letter = letter.charCodeAt(0) + 1;
-    if(letter > 64 && letter < 92){
-      letter = charCase(65, letter);
-    }else{
-      letter = charCase(97, letter);
+    letter = letter.charCodeAt(0) + 1
+    if(letter >= 65 && letter <= 91){
+      letter = rotate(65, letter);
+    }else if (letter >= 97 && letter <= 123){
+      letter = rotate(97, letter);
     }
     letter = String.fromCharCode(letter);
+    return letter;
   }
-  
   return letter;
+  
 }
-function charCase (char, letter) {
-  letter = letter - char;
-  let rotate = letter % 26;
-  letter = rotate;
-  return letter += char;
+function rotate(charCode, letter){
+  letter = letter - charCode;
+  letter = letter % 26;
+  return letter += charCode;
 }
-function isLetter(str) {
-  return str.match(/[a-z]/i);
+function isLetter(letter){
+  return letter.match(/[a-z]/i);
 }
+
 export {caesarCipher}
+
+// caesarCipher
+//  create array to store the shifted letters
+//  loop through length of the str
+//    push the shifted letter to the previous array. 
+
+// shift
+// if letter is letter
+//  convert letter to char code, and add 1
+// else
+//  return letter 
+
+// Mayus(charCode, letter)
+//  letter = letter - charcode
+//  letter = letter % charcode
+//  String.fromCharCodeAr(letter);
+
+// isLetter
+//  check if letter is not punctuation with regex.     
