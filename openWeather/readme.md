cfc6751a0f9aad7c9a9b1e9e32ab74fd1f9e24ff
David Centurion <dencen9@gmail.com>
2022-09-14 18:14:45 -0400

    readme, update sandbox

---
+++ openWeather/readme.md
@@ -0,0 +1,1 @@
+# Mocking with open weather API
--- openWeather/sandbox.js
+++ openWeather/sandbox.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const result = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${'asuncion'}&appid=${process.env.API_KEY}`)
 .then(response => response.json())
-.then(data => data.weather[0].main);
+.then(data => `Sky ${data.weather[0].main}`);
 
 
 
